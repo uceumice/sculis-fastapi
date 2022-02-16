387be359e8f04042db3fb826cc6d630d9877f6c8
Alex Tocar <alexandrutocar@gmail.com>
2022-02-16 23:11:22 +0100

    favicon added

--- app/main.py
+++ app/main.py
@@ -1,24 +1,31 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.security import http
+from fastapi.staticfiles import StaticFiles
 
-app = FastAPI()
+FAVICON_PATH = "static/favicon_io/favicon.ico"
+
+app = FastAPI(docs_url=None)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/docs", include_in_schema=False)
+async def swagger_ui_html():
+    # rewrites the default docs to include the right favicon.ico
+    return get_swagger_ui_html(
+        openapi_url="/openapi.json",
+        title="FastAPI",
+        swagger_favicon_url=FAVICON_PATH
+    )
+
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def get_favicon():
+    # sets the url for the favicon.ico
+    return FileResponse(FAVICON_PATH)
 
 
 @app.get("/")
-def read_root():
-    return {"200": "Welcome To Heroku"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.get("/fizz_buzz/{num}")
-def read_item(num: int):
-    # https: // ja.wikipedia.org / wiki / Fizz_Buzz
-    if not num % 15:
-        return {num: "Fizz Buzz"}
-    elif not num % 5 or not num % 3:
-        return {num: 'Fizz' if not num % 3 else 'Buzz'}
-    else:
-        return {num: 'Stay Silent'}
+async def read_root():
+    return {"ding": "dong"}
